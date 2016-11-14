056e8b74ff7adde82f6bddae84f5afaf912c00a3
angel <angel@ASUSF550Z>
2016-11-14 14:45:02 +0100

    Add project files.

---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(GameDevAQ.Startup))]
+
+namespace GameDevAQ
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
